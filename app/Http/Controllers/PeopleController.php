4187dda248a81f07b071aea87cf9cd9ce4f63edf
Allison Piovani <apiovani25@gmail.com>
2018-08-06 00:45:03 -0300

    Conf. people

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\People;
+use http\Env\Response;
 use Illuminate\Http\Request;
 
 class PeopleController extends Controller
@@ -14,7 +15,10 @@ class PeopleController extends Controller
      */
     public function index()
     {
-        //
+        $people = People::all();
+        return Response()->json([
+            'people' => $people
+        ]);
     }
 
     /**
