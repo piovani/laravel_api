275f4695b240d8e99993c7720f85a8f5ec4b8f68
Allison Piovani <apiovani25@gmail.com>
2019-04-12 00:09:01 -0300

    Changes in peopleController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,19 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected const messageSuccess = 'Operação efetuada com sucesso.';
+    protected const messageError   = 'Não foi possível efetuar a operação.';
+
+    protected const messsages      = [
+        200 => self::messageSuccess,
+        500 => self::messageError,
+    ];
+
+    protected function messageResponse($status = 200)
+    {
+        return Response()->json([
+            'Messagem' => self::messsages[$status],
+        ], $status);
+    }
 }
