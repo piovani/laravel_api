5af0cd559928dc5b5db9c49e20f4e315302d1fdc
Allison Piovani <allison@devsquad.com>
2019-04-22 22:31:05 -0300

    Add resource State

--- app/Domain/Localization/State/StateController.php
+++ app/Domain/Localization/State/StateController.php
@@ -5,15 +5,21 @@ namespace App\Domain\Localization\State;
 use App\Domain\Localization\City\City;
 use App\Domain\Localization\State\State;
 use App\Http\Controllers\Controller;
+use App\Domain\Localization\State\StateResource;
+
 use Illuminate\Http\Request;
 
 class StateController extends Controller
 {
     public function index(Request $request)
     {
-        $states = State::paginate($request->get('perPage') ?: 15);
-
-        return response($states);
+        return StateResource::collection(
+            State::where(function ($query) use ($request) {
+                $query->where('name', 'like', "%{$request->seach}%")
+                    ->orWhere('initials', 'like', "%{$request->seach}%");
+            })
+                ->paginate()
+        );
     }
 
     public function show($id)
