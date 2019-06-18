4c998be047e425dd9afb51b45d914892a1d8cbf9
Allison Piovani <apiovani25@gmail.com>
2019-06-17 21:45:38 -0300

    Init test

---
+++ tests/Api/TestAuth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Api;
+
+use Tests\TestCase;
+
+class TestAuth extends TestCase
+{
+    /** @test */
+    public function request_token_authentication()
+    {
+        dd('QUI');
+    }
+}
---
+++ tests/TestInit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use \PHPUnit\Framework\TestCase;
+
+class TestInit extends TestCase
+{
+    public function testSeUmIngualAUm()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
