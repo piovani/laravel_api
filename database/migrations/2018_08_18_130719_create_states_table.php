c837af90519f6d7366926edf1280af104f7050d1
Allison Piovani <apiovani25@gmail.com>
2019-06-28 19:01:11 -0300

    Changed insert States in DB.

--- database/migrations/2018_08_18_130719_create_states_table.php
+++ database/migrations/2018_08_18_130719_create_states_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Domain\Localization\State\State;
+use Faker\Factory as Faker;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -20,6 +22,43 @@ class CreateStatesTable extends Migration
             $table->string('initials', '2');
             $table->timestamps();
             $table->softDeletes();
+        });
+
+        $faker = Faker::create();
+
+        $states = collect([
+            ["Acre", "ac"],
+            ["Alagoas", "al"],
+            ["Amazonas", "am"],
+            ["Amapá", "ap"],
+            ["Bahia", "ba"],
+            ["Ceará", "ce"],
+            ["Destrito Federal", "df"],
+            ["Espírito Santo", "es"],
+            ["Goiás", "go"],
+            ["Maranhão", "ma"],
+            ["Mato Grosso", "mt"],
+            ["Mato Grosso do Sul", "ms"],
+            ["Minas Gerais", "mg"],
+            ["Pará", "pa"],
+            ["Paraíba", "pb"],
+            ["Paraná", "pr"],
+            ["Pernambuco", "pe"],
+            ["Piauí", "pi"],
+            ["Rio de Janeiro", "rj"],
+            ["Rio Grande do Norte", "rn"],
+            ["Rondônia", "ro"],
+            ["Rio Grande do Sul", "rs"],
+            ["Roraima", "rr"],
+            ["Santa Catarina", "sc"],
+            ["Segipe", "se"],
+            ["São Paulo", "sp"],
+            ["Tocantins", "to"]
+        ]);
+
+        $states->each(function ($array) use ($faker) {
+            $query = sprintf("insert into states ('id', 'name', 'initials') values ('%s', '%s', '%s')", $faker->uuid, $array[0], $array[1]);
+            DB::insert($query);
         });
     }
 
